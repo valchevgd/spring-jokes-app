82e01ec8c4752cee4b8bd448c3bae3c413791bcf
valchevgd <valchevgd@gmail.com>
2020-05-11 20:05:22 +0300

    Initial Commit

---
+++ src/test/java/com/github/valchevgd/jokesapp/SpringJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.valchevgd.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
