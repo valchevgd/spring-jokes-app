e4f35611b8d13e9eb0f6934a8aefc8625255b953
valchevgd <valchevgd@gmail.com>
2020-05-13 08:18:02 +0300

    Change to XML Configuration

--- src/main/java/com/github/valchevgd/jokesapp/SpringJokesAppApplication.java
+++ src/main/java/com/github/valchevgd/jokesapp/SpringJokesAppApplication.java
@@ -2,8 +2,10 @@ package com.github.valchevgd.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringJokesAppApplication {
 
     public static void main(String[] args) {
